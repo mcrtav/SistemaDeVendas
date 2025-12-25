1c6aa7cbe33bd497373df8f3990fd3d5bab24243
mcrtav <mcrtav@gmail.com>
2025-12-25 19:13:37 -0300

    5º commit

--- lojaapp/urls.py
+++ lojaapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("contato/", ContatoView.as_view(), name="contato"),
     path("todos-produtos/", TodosProdutoView.as_view(), name="todosprodutos"),
     path("produto/<slug:slug>", ProdutoDetalheView.as_view(), name="produtodetalhe"),
+    path("addcarro-<int:pro_id>", AddCarroView.as_view(), name="addcarro"),
    
 ]
--- lojaapp/views.py
+++ lojaapp/views.py
@@ -11,6 +11,7 @@ class HomeView(TemplateView):
         context["produto_list"] = Produto.objects.all().order_by("-id")
         return context
 
+
 class TodosProdutoView(TemplateView):
     template_name = "todosprodutos.html"
 
@@ -23,13 +24,31 @@ class TodosProdutoView(TemplateView):
 class ProdutoDetalheView(TemplateView):
     template_name = "produtodetalhe.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = self.kwargs["slug"]
+        produto = Produto.objects.get(slug=url_slug)
+        produto.visualizacao += 1
+        produto.save()
+        context["produto"] = produto
+        return context
 
 
 
-
-
-
-
+class AddCarroView(TemplateView):
+    template_name = "addprocarro.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        produto_id = self.kwargs['pro_id']
+        produto_obj = Produto.objects.get(id=produto_id)
+        carro_id = self.request.session.get("carro_id", None)
+        if carro_id:
+            carro_obj = Carro.objects.get(id=carro_id)
+        else:
+            carro_obj = Carro.objects.create(total=0)
+            self.request.session['carro_id']= carro_obj.id
+            
 
 class SobreView(TemplateView):
     template_name = "sobre.html"
@@ -37,4 +56,3 @@ class SobreView(TemplateView):
 
 class ContatoView(TemplateView):
     template_name = "contato.html"
-
